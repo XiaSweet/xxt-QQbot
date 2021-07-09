5541827fc9334e882b6fb9b50e7af69c458dfecd
XiaSweet <lolosweet@storymail.xiasweet.com>
2021-07-10 00:38:25 +0800

    优化命运轮回触发提示

--- xxt/plugins/mylh.py
+++ xxt/plugins/mylh.py
@@ -1,6 +1,6 @@
 from nonebot import on_command, require,on_keyword
 from nonebot.rule import to_me
-from nonebot.adapters.cqhttp import GROUP, Bot, Event
+from nonebot.adapters.cqhttp import Bot, Event,MessageSegment
 from nonebot.typing import T_State
 from lib.nblib.helpers import render_expression as expr
 import lib.nblib.smartlib as e
@@ -22,16 +22,16 @@ async def handle_msg(bot: Bot, event: Event, state: T_State):
             if hasattr(event,"group_id") ==False:
                 await russ_ban.finish(f'你都私信给我了我也只能跟你乐一乐了，不信你去茶话会里玩命运轮回试试？')
             else:
-                await russ_ban.finish(f'你今天堪称欧皇，小管家并不敢对你下手QAQ',at_sender=True)
+                await russ_ban.finish(f'>  '+MessageSegment.at(event.user_id)+'\n你今天堪称欧皇，小管家并不敢对你下手QAQ')
         except:
-            await russ_ban.finish(f'你今天的气势堪称欧皇，小管家并不敢对你下手QAQ',at_sender=True)
+            await russ_ban.finish(f'>  '+MessageSegment.at(event.user_id)+'\n'+f'你今天的气势堪称欧皇，小管家并不敢对你下手QAQ')
         if s=='分':
             sc=sc//60
-            await russ_ban.finish(f'您的运气值{ts}，恭喜你获得禁言{sc}{s}套餐{bg}',at_sender=True)
+            await russ_ban.finish(f'>  '+MessageSegment.at(event.user_id)+'\n'+f'您的运气值{ts}，恭喜你获得禁言{sc}{s}套餐{bg}')
         else:
-            await russ_ban.finish(f'您的运气值{ts}，恭喜你获得禁言{sc}{s}套餐{bg}',at_sender=True)
+            await russ_ban.finish('>  '+MessageSegment.at(event.user_id)+'\n'+f'您的运气值{ts}，恭喜你获得禁言{sc}{s}套餐{bg}')
     if sjz < 0.35:
-        await russ_ban.finish('恭喜你，你的运气差到跟小管家难兄难弟了，吃亏是福啊（*＾-＾*）',at_sender=True)
+        await russ_ban.finish(MessageSegment.reply(event.message_id)+'恭喜你，你的运气差到跟小管家难兄难弟了，吃亏是福啊（*＾-＾*）')
     elif 0.35<sjz and sjz<0.7:
         jy=random.randint(0,60)
         await zxjy(event.group_id,event.user_id,jy,'还算可以','(⊙﹏⊙)祝你好运')
@@ -46,8 +46,8 @@ async def handle_msg(bot: Bot, event: Event, state: T_State):
             if hasattr(event,group_id) ==False:
                 await russ_ban.finish(f'你都私信给我了我也只能跟你乐一乐了，不信你去茶话会里玩命运轮回试试？')
             else:
-                await russ_ban.finish(f'你今天堪称欧皇，小管家并不敢对你下手QAQ',at_sender=True)
+                await russ_ban.finish(MessageSegment.reply(event.message_id)+'你今天堪称欧皇，小管家并不敢对你下手QAQ')
         except:
-            await russ_ban.finish(f'你今天堪称欧皇，小管家并不敢对你下手QAQ',at_sender=True)
-        await russ_ban.finish('错误提示：\n恭喜你，由于小管家相当讨厌欧皇，所以恭喜万众瞩目的阁下获得禁言套餐Plus：88888(约1天)\
-的奖励，请等待管理员主动解除或@Ver.冬瓜萌萌，再来不便敬请谅解！\nPs:本套餐中奖概率极低，阁下今天具有大展身手的潜力，加油✧(≖ ◡ ≖✿)',at_sender=True)
+            await russ_ban.finish(MessageSegment.reply(event.message_id)+'你今天堪称欧皇，小管家并不敢对你下手QAQ')
+        await russ_ban.finish('>  '+MessageSegment.at(event.user_id)+'错误提示：\n恭喜你，由于小管家相当讨厌欧皇，所以恭喜万众瞩目的阁下获得禁言套餐Plus：88888(约1天)\
+的奖励，请等待管理员主动解除或@Ver.冬瓜萌萌，再来不便敬请谅解！\nPs:本套餐中奖概率极低，阁下今天具有大展身手的潜力，加油✧(≖ ◡ ≖✿)')
